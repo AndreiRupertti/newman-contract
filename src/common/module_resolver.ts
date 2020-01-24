67128ca707656c706401395daa45a877a11c3926
AndreiRupertti <arupertti@gmail.com>
2020-01-23 22:42:01 -0300

    Add esm require to module resolver

--- src/common/module_resolver.ts
+++ src/common/module_resolver.ts
@@ -6,10 +6,12 @@ const hasDefaultImport = (moduleToImport: any) => "default" in moduleToImport;
 const resolveDefaultImport = (moduleToImport: any) =>
     hasDefaultImport(moduleToImport) ? moduleToImport.default : moduleToImport;
 
+const esmRequire = (resolvedPath: string) => require("esm")(module)(resolvedPath);
+
 const fileToModule = (filePath: string) => {
     const resolvedPath = path.resolve(filePath);
     try {
-        const moduleToImport = require(resolvedPath);
+        const moduleToImport = esmRequire(resolvedPath);
         return resolveDefaultImport(moduleToImport);
     } catch (e) {
         throw new Error(`cannot find module at ${resolvedPath}`);
--- tests/unit/common/module_resolver.test.ts
+++ tests/unit/common/module_resolver.test.ts
@@ -1,9 +1,11 @@
 import { getModulesFromPattern } from "@src/common/module_resolver";
+import '@tests/mocks/esm';
+
 let resolvePathMock = `@tests/fixtures/modules/`
 
-jest.mock('path', jest.fn(() => ({
+jest.mock('path', () => ({
     resolve: jest.fn((str) => resolvePathMock+str)
-})));
+}));
 
 jest.mock('fast-glob', () => ({
     sync: jest.fn(() => ['esModule.ts', 'module.js'])
